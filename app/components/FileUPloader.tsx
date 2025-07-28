f169f28e13c312296b73fabfc54c0c398ba3df1c
Manoj Mondal <manojmondal9834@gmail.com>
2025-07-28 23:41:13 +0530

    Working on fileuploader form

---
+++ app/components/FileUPloader.tsx
@@ -0,0 +1,43 @@
+import React, {useCallback, useState} from 'react'
+import {useDropzone} from 'react-dropzone'
+
+interface FileUploaderProps {
+    onFileSelecet?: (file: File | null) => void
+}
+
+const FileUploader = ({onFileSelecet}: FileUploaderProps) => {
+    const [file, setFile] = useState()
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+    // Do something with the files
+    const file: File = acceptedFiles[0] || null
+    
+  }, [])
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+  return (
+    <div className='w-full gradient-border'>
+        <div {...getRootProps()}>
+        <input {...getInputProps()} />
+        <div className="space-y-4 cursor-pointer">
+            <div className="mx-auto w-16 h-16 flex items-center justify-center">
+                <img src="/icons/info.svg" alt="upload" className='size-20'/>
+            </div>
+            {file ? (
+                <div className=""></div>
+            ): (
+                <div className="">
+                    <p className='text-lg text-gray-500'>
+                        <span className='font-semibold'>
+                            Click to upload
+                        </span>
+                        or drag and drop
+                    </p>
+                    <p className='text-lg text-gray-500'>PDF (max 20 MB)</p>
+                </div>
+            )}
+        </div>
+        </div>
+    </div>
+  )
+}
+
+export default FileUploader
