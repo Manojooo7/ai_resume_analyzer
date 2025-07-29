2e7cf326a0ae0edb65800d47df5ec5665250c1aa
Manoj Mondal <manojmondal9834@gmail.com>
2025-07-29 23:27:09 +0530

    having error during upload

--- app/components/FileUPloader.tsx
+++ app/components/FileUPloader.tsx
@@ -1,37 +1,62 @@
 import React, {useCallback, useState} from 'react'
-import {useDropzone} from 'react-dropzone'
-
+import {useDropzone, type FileWithPath} from 'react-dropzone'
+import { formatSize } from '~/utils/utils'
 interface FileUploaderProps {
     onFileSelecet?: (file: File | null) => void
 }
 
 const FileUploader = ({onFileSelecet}: FileUploaderProps) => {
-    const [file, setFile] = useState()
   const onDrop = useCallback((acceptedFiles: File[]) => {
     // Do something with the files
     const file: File = acceptedFiles[0] || null
-    
-  }, [])
-  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+    onFileSelecet?.(file)
+  }, [onFileSelecet])
+  
+  const maxFileSize = 20 * 1024 * 1024
+
+  const {getRootProps, getInputProps, isDragActive, acceptedFiles} = useDropzone({
+        onDrop,
+        multiple: false,
+        accept: {'application/pdf': ['.pdf']},
+        maxSize: maxFileSize
+    });
+
+  const file = acceptedFiles[0] || null;
+
   return (
     <div className='w-full gradient-border'>
         <div {...getRootProps()}>
         <input {...getInputProps()} />
         <div className="space-y-4 cursor-pointer">
-            <div className="mx-auto w-16 h-16 flex items-center justify-center">
-                <img src="/icons/info.svg" alt="upload" className='size-20'/>
-            </div>
             {file ? (
-                <div className=""></div>
+                <div className="uploader-selected-file" onClick={(e) => e.stopPropagation()}>
+                <img src="/images/pdf.png" alt='pdf' className='size-10'/>
+                <div className="flex items-center space-x-3">
+                    <div className="text-right">
+                        <p className='text-sm text-gray-700 font-medium truncat max-w-xs'>
+                            {file.name}
+                        </p>
+                        <p className='text-sm text-gray-500'>
+                            {formatSize(file.size)}
+                        </p>
+                    </div>
+                <button className='p-2 cursor-pointer hover:bg-red-400 rounded-sm transition-colors' onClick={(e) =>{onFileSelecet?.(null)}}>
+                    <img src="/icons/cross.svg" alt="remove" className='w-4 h-4'/>
+                </button>
+                </div>
+                </div>
             ): (
                 <div className="">
+                    <div className="mx-auto w-16 h-16 flex items-center justify-center">
+                        <img src="/icons/info.svg" alt="upload" className='size-20'/>
+                    </div>
                     <p className='text-lg text-gray-500'>
                         <span className='font-semibold'>
                             Click to upload
                         </span>
                         or drag and drop
                     </p>
-                    <p className='text-lg text-gray-500'>PDF (max 20 MB)</p>
+                    <p className='text-lg text-gray-500'>PDF (max {formatSize(maxFileSize)})</p>
                 </div>
             )}
         </div>
--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -1,11 +1,94 @@
+import { prepareInstructions } from '../../constants/index';
 import React, { useState, type FormEvent } from 'react'
+import { useNavigate } from 'react-router';
 import FileUploader from '~/components/FileUPloader';
 import Navbar from '~/components/Navbar'
+import { convertPdfToImage } from '~/lib/pdfToImage';
+import { usePuterStore } from '~/lib/puter';
+import { generateUUID } from '~/utils/utils';
 
 const Upload = () => {
+    const {auth, isLoading, fs, kv, ai} = usePuterStore();
+    const navigate = useNavigate()
     const [isProccessing, setIsproccessing] = useState(false);
-    const [statusText, setStatusText] = useState('')
-    const handleSubmit = (e:FormEvent<HTMLFormElement>) =>{}
+    const [statusText, setStatusText] = useState('');
+    const [file, setFile] = useState<File | null>(null);
+    
+    const handleFileSelect = (file: File | null) =>{
+        setFile(file)
+    }
+
+    const handleAnalyze = async({companyName, jobTitle, jobDescription, file}: {companyName: string, jobTitle:string, jobDescription:string, file:File})=>{
+
+        setIsproccessing(true);
+        setStatusText("Uploading...");
+        const uploadedFile = await fs.upload([file]);
+
+        if(!uploadedFile) return setStatusText("Err faild to upload file");
+
+        setStatusText("Converting to image")
+
+        const imageFile = await convertPdfToImage(file);
+
+        if(!imageFile.file) return setStatusText("Err faild to convert the pdf to image");
+        
+        setStatusText("Uploading the image...");
+
+        const uploadedImage = await fs.upload([imageFile.file]);
+
+        if(!uploadedImage) return setStatusText("Err faild to upload image");
+
+        setStatusText("Preparing data...");
+
+        const uuid = generateUUID();
+
+        const data = {
+            id: uuid,
+            resumePath: uploadedFile.path,
+            imagePath: uploadedImage.path,
+            companyName, jobTitle, jobDescription,
+            feedBack: '',
+        }
+
+        await kv.set(`resume${uuid}`, JSON.stringify(data));
+
+        setStatusText("Analyzing...");
+
+        const feedBack = await ai.feedback(
+            uploadedFile.path,
+            prepareInstructions({jobTitle, jobDescription})
+        )
+
+        if(!feedBack) return setStatusText("Err faild to analyze resume");
+        
+        const feedBackText = typeof feedBack.message.content === 'string' ? 
+        feedBack.message.content : 
+        feedBack.message.content[0].text;
+
+        data.feedBack = JSON.parse(feedBackText);
+        await kv.set(`resume${uuid}`, JSON.stringify(data));
+        
+        setStatusText("Analyze Complete");
+
+        console.log("Analyzed Data: ", data);
+        
+    }
+    const handleSubmit = (e:FormEvent<HTMLFormElement>) =>{
+        e.preventDefault();
+        const form = e.currentTarget.closest('form');
+        if(!form) return;
+        const formData = new FormData(form)
+
+        const companyName = formData.get('company-name')?.toString() ;
+        const jobTitle = formData.get('job-title')?.toString();
+        const jobDescription = formData.get('job-description')?.toString();
+
+        if(!file) return;
+        if(!companyName || !jobTitle || !jobDescription) return;
+    
+        handleAnalyze({companyName, jobTitle, jobDescription, file})
+        
+    }
   return (
     <main className="bg-[url('/images/bg-main.svg')] bg-cover">
       <Navbar/>
@@ -15,7 +98,7 @@ const Upload = () => {
             {isProccessing ? (
                 <>
                     <h2>{statusText}</h2>
-                    <img src="./images-resume-scan.gif" alt="Scan Resumes" className='w-full'/>
+                    <img src="/images/resume-scan-2.gif" alt="Scan Resumes" className='w-full'/>
                 </>
             ): (
                 <h2>Drop your resume for ATS score and imporvemnt tips</h2>
@@ -36,7 +119,7 @@ const Upload = () => {
                     </div>
                     <div className="form-div">
                         <label htmlFor="job-description">Upload Resume</label>
-                        <FileUploader/>
+                        <FileUploader onFileSelecet={handleFileSelect}/>
                     </div>
 
                     <button className='primary-button' type='submit'>
